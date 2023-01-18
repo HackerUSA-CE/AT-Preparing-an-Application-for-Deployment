4a4ae519e4dd83f5828cacf4ddb260eba3e63873
only2pencils <89320439+only2pencils@users.noreply.github.com>
2023-01-18 00:43:04 -0500

    styled signin page but incomplete

--- frontend/src/Navigation.js
+++ frontend/src/Navigation.js
@@ -1,13 +1,12 @@
-import {Container, Navbar, Nav, NavDropdown } from 'react-bootstrap'
-import { useState, useEffect, useContext } from 'react'
+import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { useState, useEffect, useContext } from "react";
 import { useHistory } from "react-router";
-import { CurrentUser } from './contexts/CurrentUser';
+import { CurrentUser } from "./contexts/CurrentUser";
 
 function Navigation() {
+  const history = useHistory();
 
-    const history = useHistory()
-
-    const { currentUser } = useContext(CurrentUser)
+  const { currentUser } = useContext(CurrentUser);
 
     function logIn() {
         let loginActions
@@ -27,7 +26,7 @@ function Navigation() {
         return loginActions
     }
 
-    let currentBanner = logIn()
+  let currentBanner = logIn();
 
     function handleLogout(e) {
         localStorage.clear()
@@ -37,8 +36,10 @@ function Navigation() {
       
 return (
     <Navbar sticky="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand href="#home" onClick={() => history.push("/")}>The Inkwell </Navbar.Brand>
-      
+      <Navbar.Brand href="#home" onClick={() => history.push("/")}>
+        The Inkwell{" "}
+      </Navbar.Brand>
+
       <Nav className="justify-content-end">
         <Nav.Item>
           <Nav.Link eventKey="disabled" disabled>
@@ -46,29 +47,33 @@ return (
           </Nav.Link>
         </Nav.Item>
       </Nav>
-      
+
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      
+
       <Navbar.Collapse>
         <Nav>
           <Nav.Link href="#" onClick={() => history.push("/places")}>
-              Posts
+            Posts
           </Nav.Link>
           <Nav.Link href="#" onClick={() => history.push("/places/new")}>
-              Add Post
+            Add Post
           </Nav.Link>
           <NavDropdown title="Account">
-            <NavDropdown.Item href="#" onClick={() => history.push("/login")}>Sign-in</NavDropdown.Item>
-            <NavDropdown.Item href="#" onClick={handleLogout}>Sign-out</NavDropdown.Item>
+            <NavDropdown.Item href="#" onClick={() => history.push("/login")}>
+              Sign-in
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#" onClick={handleLogout}>
+              Sign-out
+            </NavDropdown.Item>
             <NavDropdown.Divider />
-            <NavDropdown.Item href="#" onClick={() => history.push("/sign-up")}>Create account</NavDropdown.Item>
+            <NavDropdown.Item href="#" onClick={() => history.push("/sign-up")}>
+              Create account
+            </NavDropdown.Item>
           </NavDropdown>
         </Nav>
       </Navbar.Collapse>
-
     </Navbar>
   );
-
-  }
+}
 
 export default Navigation;
