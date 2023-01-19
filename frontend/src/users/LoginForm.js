d54f5b42d7349d4015539e180ff9959aa8e63f8f
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2023-01-19 09:21:10 -0500

    fixing LoginForm - backend links were broken in bootstrap refactoring

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -4,9 +4,7 @@ import { CurrentUser } from "../contexts/CurrentUser";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import "bootstrap/dist/css/bootstrap.min.css";
-// import backgroundpen from "../images/backgroundpen.jpg";
 import "../css/custom.css";
-// import { FormLabel } from "react-bootstrap";
 
 function LoginForm() {
   const history = useHistory();
@@ -14,8 +12,8 @@ function LoginForm() {
   const { setCurrentUser } = useContext(CurrentUser);
 
   const [credentials, setCredentials] = useState({
-    email: "",
-    password: "",
+    email: '',
+    password: '',
   });
 
   const [errorMessage, setErrorMessage] = useState(null);
@@ -24,8 +22,7 @@ function LoginForm() {
     e.preventDefault();
 
     // const response = await fetch(`http://localhost:5000/authentication/`, {
-    const response = await fetch(
-      `${process.env.REACT_APP_SERVER_URL}authentication/`,
+    const response = await fetch(`${process.env.REACT_APP_SERVER_URL}authentication/`, 
       {
         method: "POST",
         headers: {
@@ -38,22 +35,16 @@ function LoginForm() {
     const data = await response.json();
 
     if (response.status === 200) {
-      setCurrentUser(data.user);
-      localStorage.setItem("token", data.token);
-      history.push(`/places`);
+      setCurrentUser(data.user)
+      localStorage.setItem('token', data.token)
+      history.push(`/places`)
     } else {
-      setErrorMessage(data.message);
+      setErrorMessage(data.message)
     }
   }
 
   return (
     <div className="bg-Img">
-      {/* <img
-        className="img-fluid"
-        src={backgroundpen}
-        alt="backgroundpen"
-        height="100vh"
-      /> */}
       <main>
         <h1 style={{ color: "black" }}>Sign-in</h1>
         {errorMessage !== null ? (
@@ -61,10 +52,15 @@ function LoginForm() {
             {errorMessage}
           </div>
         ) : null}
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email Address</Form.Label>
-            <Form.Control type="email" placeholder="Enter Email" />
+            <Form.Control 
+              required 
+              type="email" 
+              value={credentials.email} 
+              onChange={ e => setCredentials( { ...credentials, email: e.target.value })}
+              placeholder="Enter Email" />
             <Form.Text className="text-muted">
               We will never sell or share your email with anyone else.
             </Form.Text>
@@ -72,13 +68,20 @@ function LoginForm() {
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Enter Password" />
+            <Form.Control 
+              required
+              type="password" 
+              value={credentials.password} 
+              onChange={ e => setCredentials( { ...credentials, password: e.target.value })}
+              placeholder="Enter Password" />
           </Form.Group>
+
           <Form.Group className="mb-3" controlId="formBasicCheckbox">
             <Form.Check type="checkbox" label="Remember Me" />
           </Form.Group>
+
           <div className="text-center">
-            <Button variant="dark">Sign-in</Button>
+            <Button variant="dark" type="submit">Sign-in</Button>
           </div>
         </Form>
       </main>
@@ -86,52 +89,3 @@ function LoginForm() {
   );
 }
 export default LoginForm;
-
-//<div>
-//<img src="backgroundpen.jpg" alt="backgroundpen"></img>
-//</div>;
-// }
-// {
-/* <form onSubmit={handleSubmit}>
-        <div className="row">
-          <div className="col-sm-6 form-group">
-            <label htmlFor="email">Email Address</label>
-
-            <input
-              type="email"
-              required
-              value={credentials.email}
-              onChange={(e) =>
-                setCredentials({ ...credentials, email: e.target.value })
-              }
-              className="form-control"
-              id="email"
-              name="email"
-            />
-          </div>
-          <div className="col-sm-6 form-group">
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              required
-              value={credentials.password}
-              onChange={(e) =>
-                setCredentials({ ...credentials, password: e.target.value })
-              }
-              className="form-control"
-              id="password"
-              name="password"
-            />
-          </div>
-        </div>
-        {/* <input className="btn btn-primary" type="submit" value="Sign-in" /> */
-// }
-
-//   {/* </form> */} */}
-// {/* // </main> */}
-// {
-/* ); */
-// }
-// {
-/* } */
-// }
